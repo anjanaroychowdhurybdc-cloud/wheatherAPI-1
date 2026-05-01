0099b46a934d0dd896d2ad95c780231706967878
Anjana Roychowdhury <anjanaroychowdhury.bdc@gmail.com>
2026-05-02 00:13:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
+import Home from "./components/Home";
+import Hourly from "./components/Hourly";
+import Daily from "./components/Daily";
+import SavedLocations from "./components/SavedLocations";
+import Settings from "./components/Settings";
+
+const App = () => {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hourly" element={<Hourly />} />
+        <Route path="/daily" element={<Daily />} />
+        <Route path="/locations" element={<SavedLocations />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Layout>
+  );
+};
+
+export default App;
