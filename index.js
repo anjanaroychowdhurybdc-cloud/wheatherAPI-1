4682fbec6f5f9d7e304f9787fe8b7278c6ac520c
Anjana Roychowdhury <anjanaroychowdhury.bdc@gmail.com>
2026-05-02 00:09:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import { WeatherProvider } from "./context/WeatherContext";
+import "./WeatherApp.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <HashRouter>
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
+  </HashRouter>
+);
